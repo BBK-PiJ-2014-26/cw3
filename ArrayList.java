dc6735fb72ff8f1163a65b82b5b97d08b78f6ac1
BBK-PiJ-2014-26 <gmoore04@dcs.bbk.ac.uk>
2015-01-04 17:44:16 +0000

    Added size() method.

--- ArrayList.java
+++ ArrayList.java
@@ -1,18 +1,26 @@
 public class ArrayList implements List {
 
 	private Object[] list;
-	private int size;
 
-	public ArrayList(int size) {
-		this.list = new Object[size];
-		this.size = size;
+	public ArrayList(int length) {
+		this.list = new Object[length];
 	}
 
 	public boolean isEmpty() {
 		boolean result = true;
-		for (int i = 0; i < this.size; i++) {
+		for (int i = 0; i < this.list.length; i++) {
 			if (list[i] != null) {
 				result = false;
+			}
+		}
+		return result;
+	}
+
+	public int size() {
+		int result = 0;
+		for (int i = 0; i < this.list.length; i++) {
+			if (list[i] != null) {
+				result = result + 1;
 			}
 		}
 		return result;
