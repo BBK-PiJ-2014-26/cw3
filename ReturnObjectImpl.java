17da344289b63a02c2f315075ef023493777c22e
BBK-PiJ-2014-26 <gmoore04@dcs.bbk.ac.uk>
2015-01-04 16:47:54 +0000

    Added hasError() method

--- ReturnObjectImpl.java
+++ ReturnObjectImpl.java
@@ -1,5 +1,23 @@
 public class ReturnObjectImpl implements ReturnObject {
 
 	private ErrorMessage error;
+	private Object returnObject;
+
+	public boolean hasError() {
+		boolean result;
+		switch (this.error) {
+			case NO_ERROR:				result = false;
+										break;
+			case EMPTY_STRUCTURE:		result = true;
+										break;
+			case INDEX_OUT_OF_BOUNDS:	result = true;
+										break;
+			case INVALID_ARGUMENT:		result = true;
+										break;
+			default:					result = false;
+										break;
+		}
+		return result;
+	}
 
 }
