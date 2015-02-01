a6258e5fa685d689288c96df9bb339ab4c7280b1
BBK-PiJ-2014-26 <gmoore04@dcs.bbk.ac.uk>
2015-02-01 17:35:59 +0000

    First commit

---
+++ ImprovedStackImpl.java
@@ -0,0 +1,32 @@
+public class ImprovedStackImpl implements ImprovedStack {
+
+	public boolean isEmpty() {
+		boolean result = false;
+		return result;
+	}
+
+	public int size() {
+		int result = 0;
+		return result;
+	}
+
+	public void push(Object item) {}
+
+	public ReturnObject top() {
+		ReturnObject result = new ReturnObjectImpl();
+		return result;
+	}
+
+	public ReturnObject pop() {
+		ReturnObject result = new ReturnObjectImpl();
+		return result;
+	}
+
+	public ImprovedStack reverse() {
+		ImprovedStack result = new ImprovedStackImpl();
+		return result;
+	}
+
+	public void remove(Object object) {}
+
+}
