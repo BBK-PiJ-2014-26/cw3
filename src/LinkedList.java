bdbce660e9be38499ea8679a229603188c0056a8
BBK-PiJ-2014-26 <gmoore04@dcs.bbk.ac.uk>
2015-02-02 02:16:27 +0000

    Added javadoc comments

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -1,14 +1,36 @@
+/**
+ * Implements the List Interface using a singly Linked List structure.
+ * @author Gareth Moore
+ */
 public class LinkedList implements List {
+	/**
+	 * Holds the value in each LinkedList node.
+	 */
 	private Object element;
+	/**
+	 * Refers to the position in the list of each node.
+	 */
 	private int index;
+	/**
+	 * Pointer to the following node in the list.
+	 */
 	private LinkedList next;
 
+	/**
+	 * Constructor instantiates a LinkedList. First node is simply a pointer to the actual list.
+	 * Index is set to -1 to identify this is merely a pointer.
+	 */
 	public LinkedList() {
 		this.element = null;
 		this.index = -1;
 		this.next = null;
 	}
 
+	/**
+	 * Constructor adds all subsequent nodes to the list.
+	 * @param index is the index of the preceding node.
+	 * @param element is the value to be stored in the list.
+	 */
 	public LinkedList(Object element, int index) {
 		this.element = element;
 		this.index = index + 1;
@@ -76,6 +98,10 @@ public class LinkedList implements List {
 		return result;
 	}
 
+	/**
+	 * Adds one to the index of each node from the starting node to the end of the list.
+	 * Used when a new node is added in the middle of a list.
+	 */
 	public void increaseIndex() {
 		this.index = this.index + 1;
 		if (this.next != null) {
@@ -83,6 +109,10 @@ public class LinkedList implements List {
 		}
 	}
 
+	/**
+	 * Subtracts one from the index of each node from the starting node to the end of the list.
+	 * Used when a node is removed from the list.
+	 */
 	public void decreaseIndex() {
 		this.index = this.index - 1;
 		if (this.next != null) {
